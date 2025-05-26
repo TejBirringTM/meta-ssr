b6a6ca9ce58d2aa90bd1424396ed5d92fcf5dae7
Tej Birring <tejbirring@gmail.com>
2025-05-26 03:47:14 +0100

    Improve pre-initialisation SQL scripts.

--- infra/scripts/00-initialise-db.sql
+++ infra/scripts/00-initialise-db.sql
@@ -10,8 +10,8 @@ CREATE SCHEMA access_control_sch;
 -- Create user: ORM Admin (to execute migrations).
 CREATE ROLE be_orm_admin_user WITH LOGIN PASSWORD '${BE_ORM_ADMIN_PWD}';
 GRANT CONNECT ON DATABASE app_db TO be_orm_admin_user;
-GRANT CREATE, USAGE ON SCHEMA access_control_sch TO be_orm_admin_user;
+GRANT ALL PRIVILEGES ON SCHEMA access_control_sch TO be_orm_admin_user;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA access_control_sch TO be_orm_admin_user;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA access_control_sch TO be_orm_admin_user;
--- set default schema
+-- Set default schema
 ALTER USER be_orm_admin_user SET search_path TO access_control_sch;
--- infra/scripts/01-create-user-for-server.sql
+++ infra/scripts/01-create-user-for-server.sql
@@ -3,5 +3,5 @@ CREATE ROLE be_server_user WITH LOGIN PASSWORD '${BE_SERVER_PWD}';
 GRANT CONNECT ON DATABASE app_db TO be_server_user;
 GRANT USAGE ON SCHEMA access_control_sch TO be_server_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA access_control_sch TO be_server_user;
--- set default schema
+-- Set default schema
 ALTER USER be_server_user SET search_path TO access_control_sch;
